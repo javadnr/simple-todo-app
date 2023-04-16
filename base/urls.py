423277af4385e59581f8978b692063ea9447487d
javad <javadnamavar554@gmail.com>
2023-04-16 12:57:01 +0430

    edit and delete post added

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(),name="list"),
     path('<int:pk>/detail/',views.PostDetailView.as_view(),name='post_detail'),
+    path('<int:pk>/update/', views.PostUpdateView.as_view(),name="post_update"),
+    path('<int:pk>/delete/',views.PostDeleteView.as_view(),name="post_delete"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, CreateView, DeleteView, UpdateView,TemplateView
 from . models import PostModel
+from django.urls import reverse_lazy
 
 
 class PostListView(ListView):
@@ -13,3 +14,14 @@ class PostDetailView(DetailView):
     template_name = "detail.html"
     context_object_name= 'post'
     fields = "__all__"
+
+class PostUpdateView(UpdateView):
+    model = PostModel
+    template_name = "update.html"
+    fields = ["title", "body",'done']
+
+
+class PostDeleteView(DeleteView):
+    model = PostModel
+    template_name = "delete.html"
+    success_url = reverse_lazy("list")
