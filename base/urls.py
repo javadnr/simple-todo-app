852ea9bda28700b421bd92ea045caecdbc6490f1
javad <javadnamavar554@gmail.com>
2023-04-16 12:50:18 +0430

    detail view added

--- base/urls.py
+++ base/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(),name="list"),
+    path('<int:pk>/detail/',views.PostDetailView.as_view(),name='post_detail'),
 ]
--- base/views.py
+++ base/views.py
@@ -7,3 +7,9 @@ class PostListView(ListView):
     model = PostModel
     template_name = "list.html"
     context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = PostModel
+    template_name = "detail.html"
+    context_object_name= 'post'
+    fields = "__all__"
