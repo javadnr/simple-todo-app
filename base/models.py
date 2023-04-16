ed923ee54648ab20274e104b65a35800390fb9d0
javad <javadnamavar554@gmail.com>
2023-04-16 12:22:16 +0430

    post model created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PostModel(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField(null=True, blank=True)
+    created =models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    done = models.BooleanField(default = False)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    
+    def get_absolute_url(self):    
+        return reverse("post_detail", args=[str(self.id)])
